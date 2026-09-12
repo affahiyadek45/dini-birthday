76ccb7177a977cb83088355eae01a339c23db114
affahiyadek45 <affahiyadek45@gmail.com>
2026-09-13 00:07:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -158,7 +158,7 @@ document.addEventListener("DOMContentLoaded", () => {
             <div class="birthday-gate-door birthday-gate-door-right" aria-hidden="true"></div>
             <div class="birthday-gate-card">
                 <div class="birthday-gate-kicker">♡ Sebelum masuk ♡</div>
-                <h2>Setelah Berpisah, Kapan kita pertama kali bertemu ?</h2>
+                <h2>Setelah Berpisah, Kapan pertama kali kita bertemu kembali ?</h2>
                 <p class="birthday-gate-subtitle"></p>
                 <input
                     id="birthdayGateInput"
